74b24aba05e37a7f05f9b54d5f46af95ec9254cf
Jeremy Potterfield <Jeremy.Potterfield@gmail.com>
2022-08-02 12:53:28 -0400

    changed fetch to get https rather than http

--- src/javascript/weather-module-fns/getWeather.js
+++ src/javascript/weather-module-fns/getWeather.js
@@ -5,7 +5,7 @@ async function getWeather(location) {
   //     "&APPID=99b0b5395e6ca6d7d0df00a1cddea38f"
   // );
   const response = await fetch(
-    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
       location +
       "&APPID=99b0b5395e6ca6d7d0df00a1cddea38f",
     { mode: "cors" }
