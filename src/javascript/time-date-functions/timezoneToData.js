c4d771c8e65d3affdf8a0441d6aab884e8ea205b
Jeremy Potterfield <Jeremy.Potterfield@gmail.com>
2022-08-04 12:00:09 -0400

    fixed timezone formula so that it calculates local date based off of UTC date, not user's date

--- src/javascript/time-date-functions/timezoneToData.js
+++ src/javascript/time-date-functions/timezoneToData.js
@@ -7,10 +7,24 @@ import { add, format, getTime, sub } from "date-fns";
 function timezoneToData(timezone) {
   //-36000
   let time = getUTCHour();
+  console.table(time);
 
   let offset = timezone / 36; //-10
+  console.table(offset);
 
   let newTime = time + offset; //4:31
+  console.table(newTime);
+
+  let testDate = new Date();
+  let testUTCDate = testDate.getUTCFullYear();
+  let UTCMonth = testDate.getUTCMonth();
+  let UTCDate = testDate.getUTCDate();
+  let UTCYear = testDate.getUTCFullYear();
+  console.table(UTCDate, UTCMonth, UTCYear);
+  let newUTC = new Date(UTCYear, UTCMonth, UTCDate);
+  console.log("new UTC is " + newUTC);
+  let formatNewUTC = format(newUTC, "PP");
+  console.table(formatNewUTC);
 
   let datetimeObj = {
     localDate: "",
@@ -23,7 +37,7 @@ function timezoneToData(timezone) {
   if (newTime > 2400) {
     console.log(newTime);
     console.log("greater than 2400");
-    let date = new Date();
+    let date = newUTC;
     date = add(date, {
       days: 1,
     });
@@ -37,7 +51,7 @@ function timezoneToData(timezone) {
     datetimeObj.formattedTime =
       formatTime(newTime) + " " + getMeridian(newTime);
   } else if (newTime < 0) {
-    let date = new Date();
+    let date = newUTC;
     date = sub(date, {
       days: 1,
     });
@@ -50,7 +64,7 @@ function timezoneToData(timezone) {
     datetimeObj.formattedTime =
       formatTime(newTime) + " " + getMeridian(newTime);
   } else {
-    let date = new Date();
+    let date = newUTC;
     date = format(date, "PP");
     datetimeObj.localDate = date;
     datetimeObj.localTime = newTime;
